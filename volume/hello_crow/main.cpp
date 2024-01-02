0e47195d29979326e36e539934af4b22e30aacc4
hans <hans0811>
2024-01-02 17:12:50 +0800

    change dokcer files

--- volume/hello_crow/main.cpp
+++ volume/hello_crow/main.cpp
@@ -1,13 +1,63 @@
 #include <iostream>
 #include "crow_all.h"
 using namespace std;
+using namespace crow;
+
+void sendFile(response &res, string filename, string contentType){
+    ifstream in("../public/"+ filename, ifstream::in);
+    if(in){
+        ostringstream contents;
+        contents << in.rdbuf();
+        in.close();
+        res.set_header("Content-Type", contentType);
+        res.write(contents.str());
+    } else {
+        res.code = 404;
+        res.write("Not find page");
+    }
+    res.end();
+}
+
+void sendHtml(response &res, string filename){
+    sendFile(res, filename + ".html", "text/html");
+}
+
+void sendScript(response &res, string filename){
+    sendFile(res, "scripts/" + filename, "text/javascript");
+}
+
+void sendImage(response &res, string filename){
+    sendFile(res, "images/" + filename, "image/jpeg");
+}
+
+void sendStyle(response &res, string filename){
+    sendFile(res, "styles/" + filename, "text/css");
+}
 
 int main(int argc, char* argv[]) {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")(
-            [](){
-                return "<div><h1>Hello, Crow.</h1></div>";
+    CROW_ROUTE(app, "/styles/<string>")
+            ([](const request &req, response &res, string filename){
+                sendStyle(res, filename);
+            });
+    CROW_ROUTE(app, "/scripts/<string>")
+            ([](const request &req, response &res, string filename){
+                sendScript(res, filename);
+            });
+    CROW_ROUTE(app, "/images/<string>")
+            ([](const request &req, response &res, string filename){
+                sendImage(res, filename);
+            });
+
+    CROW_ROUTE(app, "/")
+    ([](const request &req, response &res){
+        sendHtml(res, "index");
+            });
+
+    CROW_ROUTE(app, "/about")
+            ([](const request &req, response &res){
+                sendHtml(res, "about");
             });
 
     char* port = getenv("PORT");
